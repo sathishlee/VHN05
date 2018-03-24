aab861383a3b1fb36f8b1f4cf79b6d84cc9dc384
suthishan <suthishan@satvatinfosol.com>
2018-03-24 18:41:45 +0530

    Maps Activity

---
+++ app/src/main/java/com/unicef/vhn/activity/MothersPrimaryRecordsActivity.java
@@ -0,0 +1,25 @@
+package com.unicef.vhn.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.unicef.vhn.R;
+
+/**
+ * Created by Suthishan on 20/1/2018.
+ */
+
+public class MothersPrimaryRecordsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.primary_details);
+        initUI();
+
+    }
+
+    public void initUI(){
+
+    }
+}
